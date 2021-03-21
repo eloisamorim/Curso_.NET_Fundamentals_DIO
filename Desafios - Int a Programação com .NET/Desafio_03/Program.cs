438ec423f3da9e3bd5a2e112406cc92a1a1104ad
eloisamorim <eloisa.amorim1@hotmail.com>
2021-03-21 14:32:26 -0300

    finalizando desafio 3

--- Desafios - Int a Programação com .NET/Desafio_03/Program.cs
+++ Desafios - Int a Programação com .NET/Desafio_03/Program.cs
@@ -2,18 +2,15 @@
 
 class minhaClasse
 {
-    static void Main(string[] args)
-    {
-        for (int i = 0; i < 2; i++)
-        {
-            string[] entrada = Console.ReadLine().Split(" ");
+	static void Main(string[] args)
+	{
+		string[] entrada = Console.ReadLine().Split(" ");
 
-            double H = double.Parse(entrada[0]);
-            double P = double.Parse(entrada[1]);
+		double H = double.Parse(entrada[0]);
+		double P = double.Parse(entrada[1]);
 
-            double media = H / P;
+		double media = H / P;
 
-            Console.WriteLine(Math.Round(media, 2));
-        }
-    }
+		Console.WriteLine(Math.Round(media,2));
+	}
 }
