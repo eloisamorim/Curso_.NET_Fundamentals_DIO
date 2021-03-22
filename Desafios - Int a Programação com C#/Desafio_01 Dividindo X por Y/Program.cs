4c2aeca21715a1ee7190a348e5e9f756479be6b3
eloisamorim <eloisa.amorim1@hotmail.com>
2021-03-21 21:49:17 -0300

    add desafios C#

---
+++ Desafios - Int a Programação com C#/Desafio_01 Dividindo X por Y/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Desafio
+{
+    static void Main(string[] args)
+    {
+        int n = Int32.Parse(Console.ReadLine());  // Entrada N para quantidade de pares que serão lidos
+
+        for (int i = 0; i < n; i++)
+        {
+            string[] entrada = Console.ReadLine().Split(" ");
+
+            float x = float.Parse(entrada[0]);
+            float y = float.Parse(entrada[1]);
+
+            float divisao = x / y;
+
+            if (y == 0)
+            {
+                Console.WriteLine("divisao impossivel");
+            }
+            else
+            {
+                Console.WriteLine(divisao.ToString("N1"));
+            }
+        }
+    }
+}
---
+++ Desafios - Int a Programação com C#/Desafio_02 Distância/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Desafio
+{
+    static void Main()
+    {
+        int quilometros = Int32.Parse(Console.ReadLine());
+        int minutos = quilometros * 2; // Em t = 60min, d = 30Km.  Logo, t = d(6/3);   minutos = quilometros * 2;
+
+        Console.WriteLine(minutos + " minutos");
+    }
+}
